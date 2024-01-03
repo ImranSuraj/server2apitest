5e4705cd467a42bdf1632a47eca0f38ea4fdaa21
Imran <imranwani00000@gmail.com>
2024-01-03 07:46:11 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+// Create an instance of express
+const app = express();
+
+
+// Define a route for the homepage
+app.get('/favourite', (req, res) => {
+  res.json([
+    {
+    
+        "poster": "https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+        "title": "Ultimate Avengers II",
+        "type": "movie",
+        "year": "2006",
+        "imdb_id": "9",
+        "comment": "good",
+        "id": 1
+      },
+      {
+        "poster": "https://m.media-amazon.com/images/M/MV5BZWQwZTdjMDUtNTY1YS00MDI0LWFkNjYtZDA4MDdmZjdlMDRlXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
+        "title": "The Avengers",
+        "type": "series",
+        "year": "1961–1969",
+        "imdb_id": "8",
+        "comment": "nice movie",
+        "id": 2
+      
+    }
+  ])
+});
+
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
