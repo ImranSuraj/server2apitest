89912d2b032888335154a35144c0cfd07f242807
Imran <imranwani00000@gmail.com>
2024-01-03 08:13:41 +0530

    my first comment

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Define a route for the homepage
-app.get('/favourite', (req, res) => {
-  res.json([
+app.get('/', (req, res) => {
+  res.json(
     {
       "poster": "https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
       "title": "Ultimate Avengers II",
@@ -14,18 +14,9 @@ app.get('/favourite', (req, res) => {
       "imdb_id": "9",
       "comment": "good",
       "id": 1
-    },
-    {
-      "poster": "https://m.media-amazon.com/images/M/MV5BZWQwZTdjMDUtNTY1YS00MDI0LWFkNjYtZDA4MDdmZjdlMDRlXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
-      "title": "The Avengers",
-      "type": "series",
-      "year": "1961–1969",
-      "imdb_id": "8",
-      "comment": "nice movie",
-      "id": 2
     }
-  ]);
-});
+  )
+  });
 
 // Start the server
 
